0a66ab6342e27f58973f31ad88a99d485c7d2465
Vraj Panchal <vrajpanchal110106@gmail.com>
2025-12-22 18:42:55 +0530

    Enable user authentication routes in userRouter.js
    Uncommented user router code to enable user authentication routes.

--- Drizzle_Admin_User_Auth/routes/userRouter.js
+++ Drizzle_Admin_User_Auth/routes/userRouter.js
@@ -1,30 +1,3 @@
-// import express from "express";
-// import {userTable} from "../src/db/schema/user.js";
-// import {registerUser,loginUser,logoutUser , getApprovedUser} from "../controllers/userController.js";
-// import {isUserLoggedIn} from "../middlewares/isUserLoggedIn.js";
-// import {db} from "../config/db.js";
-// const router = express.Router();
-
-
-// router.get("/",function(req,res){
-//     res.send("hey userRouter working grate . ");
-// });
-
-// import { uploadProduct } from "../controllers/uploadController.js";
-
-// router.post("/upload", isUserLoggedIn, upload.single("image"), uploadProduct);
-
-// router.post("/userRegister",registerUser);
-
-// router.post("/userLogin",loginUser);
-
-// router.get("/logout",logoutUser);
-
-// router.get("/approvedUser",isUserLoggedIn,getApprovedUser);
-
-// export default router;
-
-
 import express from "express";
 import { userTable } from "../src/db/schema/user.js";
 import { registerUser, loginUser, logoutUser, getApprovedUser } from "../controllers/userController.js";
